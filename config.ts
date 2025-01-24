e4c077b6c79499fe2f0d4ec2617f26993f2df427
holamite <olamideadebara@gmail.com>
2025-01-24 22:58:14 +0100

    Add: Setup the wallet connect

--- config.ts
+++ config.ts
@@ -1,10 +1,11 @@
-import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { kairos } from "viem/chains";
 
-export const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-})
+const config = getDefaultConfig({
+  appName: "Alpha",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [kairos],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+export default config;
---
+++ src/constant/constant.tsx
@@ -0,0 +1,653 @@
+export const CONTRACT_ADDRESS = "0x934932752EDDeb6150e412E04D747bd974164A7d";
+export const CONTRACT_ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "isAvailable",
+        type: "bool",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "hourlyRate",
+        type: "uint256",
+      },
+    ],
+    name: "ConsultationAvailabilitySet",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "client",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
+      },
+    ],
+    name: "ConsultationBooked",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+    ],
+    name: "CreatorRegistered",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+    ],
+    name: "ProfileUpdated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "toolId",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+    ],
+    name: "ToolAdded",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "buyer",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "toolId",
+        type: "uint256",
+      },
+    ],
+    name: "ToolPurchased",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_category",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "_price",
+        type: "uint256",
+      },
+    ],
+    name: "addTool",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_timestamp",
+        type: "uint256",
+      },
+    ],
+    name: "bookConsultation",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "consultationInfos",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "isAvailable",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "hourlyRate",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "creatorEarnings",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "totalEarnings",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "toolSales",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "consultationRevenue",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "creatorProfiles",
+    outputs: [
+      {
+        internalType: "address",
+        name: "creatorAddress",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "bio",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "photoHash",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "twitterHandle",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "githubHandle",
+        type: "string",
+      },
+      {
+        internalType: "bool",
+        name: "isRegistered",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "creatorTools",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "creator",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "description",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "category",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "price",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "isActive",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_bio",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_photoHash",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_twitterHandle",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_githubHandle",
+        type: "string",
+      },
+    ],
+    name: "editProfile",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+    ],
+    name: "getConsultationInfo",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "bool",
+            name: "isAvailable",
+            type: "bool",
+          },
+          {
+            internalType: "uint256",
+            name: "hourlyRate",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256[]",
+            name: "availableSlots",
+            type: "uint256[]",
+          },
+        ],
+        internalType: "struct Creator.ConsultationInfo",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+    ],
+    name: "getCreatorEarnings",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "totalEarnings",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "toolSales",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "consultationRevenue",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct Creator.CreatorEarnings",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+    ],
+    name: "getCreatorProfile",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "creatorAddress",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "bio",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "photoHash",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "twitterHandle",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "githubHandle",
+            type: "string",
+          },
+          {
+            internalType: "bool",
+            name: "isRegistered",
+            type: "bool",
+          },
+        ],
+        internalType: "struct Creator.CreatorProfile",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+    ],
+    name: "getCreatorTools",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "id",
+            type: "uint256",
+          },
+          {
+            internalType: "address",
+            name: "creator",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "description",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "category",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "price",
+            type: "uint256",
+          },
+          {
+            internalType: "bool",
+            name: "isActive",
+            type: "bool",
+          },
+        ],
+        internalType: "struct Creator.Tool[]",
+        name: "",
+        type: "tuple[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_creator",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "_toolId",
+        type: "uint256",
+      },
+    ],
+    name: "purchaseTool",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_name",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_bio",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_photoHash",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_twitterHandle",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "_githubHandle",
+        type: "string",
+      },
+    ],
+    name: "registerCreator",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bool",
+        name: "_isAvailable",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "_hourlyRate",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256[]",
+        name: "_availableSlots",
+        type: "uint256[]",
+      },
+    ],
+    name: "setConsultationAvailability",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "toolPurchases",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
